407ff49944b2894648ac1efd430f2ed53b582806
stely69 <you@example.com>
2025-04-07 12:59:34 -0500

    fix: update title in Admin view to reflect correct application name

--- public/views/Admin/Admin.php
+++ public/views/Admin/Admin.php
@@ -20,7 +20,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Administración - SENA</title>
+    <title>Panel de Administración - GEDAC</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <!--Colores personalizadas-->
     <script>
