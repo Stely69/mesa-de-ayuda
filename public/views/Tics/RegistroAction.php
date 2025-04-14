207e12b1e37e2063a60d755e0523eb50095dfd6e
stely69 <you@example.com>
2025-04-14 17:04:26 -0500

    feat: add RegistroAction.php for user creation functionality

---
+++ public/views/Tics/RegistroAction.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once __DIR__ . '/../../../Controller/UserController.php';
+
+
+    $create = new UserController();
+    $create->CreateuserAuxiliar($_POST['documento'], $_POST['nombres'],$_POST['apellido'], $_POST['correo'], $_POST['contraseña'], $_POST['rol']);
