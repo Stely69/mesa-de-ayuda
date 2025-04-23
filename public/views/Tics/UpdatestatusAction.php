c1d8b63b7b2207a911b480ae2656d827fe91a674
stely69 <you@example.com>
2025-04-23 14:57:10 -0500

    feat: add UpdatestatusAction.php to handle case status updates

---
+++ public/views/Tics/UpdatestatusAction.php
@@ -0,0 +1,5 @@
+<?php
+    require_once __DIR__ . '/../../../Controller/CasoController.php';
+
+    $statu =  new CasoController();
+    $statu->updateCasoStatus($_POST['caso_id'],$_POST['estado_id']);
