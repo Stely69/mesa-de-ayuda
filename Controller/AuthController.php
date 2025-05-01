1c44f5dd416f9679908d16223f053a1e9cbd3762
stely69 <you@example.com>
2025-05-01 18:10:44 -0500

    fix: correct redirect path for instructor role in AuthController

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -44,7 +44,7 @@
                     header('Location: ../admin/admin');
                     break;
                 case 2:
-                    header('Location: ../Instr/instructores');
+                    header('Location: ../Instr/IncioInst');
                     break;
                 case 3:
                    header('Location: ../Tics/Tics');
