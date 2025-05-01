2539e8881c90e853669185b2a10a836b66058df2
stely69 <you@example.com>
2025-05-01 18:12:06 -0500

    feat: enhance updateCasoStatus method to include user ID and observations

--- public/views/Tics/UpdatestatusAction.php
+++ public/views/Tics/UpdatestatusAction.php
@@ -2,4 +2,4 @@
     require_once __DIR__ . '/../../../Controller/CasoController.php';
 
     $statu =  new CasoController();
-    $statu->updateCasoStatus($_POST['caso_id'],$_POST['estado_id']);
+    $statu->updateCasoStatus($_POST['caso_id'],$_POST['estado_id'],$_POST['usuario_id'],$_POST['observaciones']);
